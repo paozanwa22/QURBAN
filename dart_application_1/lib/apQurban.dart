5ef0b66209851ba03fc1df1f9d8bfdaac228f2e3
paozanwa22 <fausanwadi@gmail.com>
2021-07-31 18:13:37 +0800

    QURBAN CRUD dart v1

---
+++ dart_application_1/lib/apQurban.dart
@@ -0,0 +1,97 @@
+import 'dart:io';
+
+import 'hewanQurban.dart';
+
+class ProgramHewanQurban {
+  // Penyimpan list hewan qurban
+  var hewanQurban = <HewanQurban>[];
+  var runing = true;
+
+  void showMenu() {
+    print('');
+    print('Menu');
+    print('1. daftarkan hewan Qurban');
+    print('2. lihat daftar hewan Qurban');
+    print('3. update data hewan qurban');
+    print('4. hapus data hewan qurban');
+    print('5. bersihkan data');
+    print('Pilih menu : \n');
+    var nomor_pilihan = stdin.readLineSync()!;
+    selectedMenu(int.parse(nomor_pilihan));
+  }
+
+  void start() {
+    while (runing) {
+      showMenu();
+    }
+  }
+
+  void selectedMenu(int nomor_pilihan) {
+    switch (nomor_pilihan) {
+      case 1:
+        // Fungsi input
+        var _newHewan = HewanQurban()
+          ..create();
+        // Tampung hewan qurban yang ditambahkan ke list hewan qurban  
+        hewanQurban.add(_newHewan);
+        showMenu();
+
+        break;
+      case 2:
+        // fungsi tampilkan list qurban
+        stdout.write('Daftar hewan qurban \n');
+        hewanQurban.forEach((hewan) {
+          print(hewan.cetak());
+        });
+        showMenu();
+
+        break;
+      case 3:
+        // ubah data
+        print('Daftar hewan Qurban');
+        for(var i = 0; i < hewanQurban.length; i++){
+          print('$i, ${hewanQurban[i].cetak()}');
+        }
+
+        print('\nUpdate data ke: \n');
+        var u = int.parse(stdin.readLineSync()!);
+        for(var i = 0; i < hewanQurban.length; i++){
+          if( u == i ){
+            print('$i, ' + hewanQurban[i].cetak());
+            hewanQurban[i].runUpdate();
+          }
+        }
+
+        showMenu();
+        break;
+      case 4:
+        //  hapus data
+        print('Daftar hewan Qurban');
+        for(var i = 0; i < hewanQurban.length; i++){
+          print('$i, ${hewanQurban[i].cetak()}');
+        }
+
+        print('\nHapus data ke: \n');
+        var u = int.parse(stdin.readLineSync()!);
+        for(var i = 0; i < hewanQurban.length; i++){
+          if( u == i ){
+            print('$i, ' + hewanQurban[i].cetak());
+            hewanQurban.removeAt(i);
+          }
+        }
+
+        showMenu();
+        break;
+      case 5: 
+        hewanQurban.clear();
+        break;
+      default:
+        runing = false;
+        break;
+    }
+  }
+}
+
+void main() {
+  ProgramHewanQurban().start();
+}
---
+++ dart_application_1/lib/hewanQurban.dart
@@ -0,0 +1,114 @@
+  import 'dart:io';
+
+  List<String> jenisHewanQurban = ['sapi', 'onta', 'kambing'];
+
+  class HewanQurban {
+    String? namaHewan;
+    int? jumlah;
+    int? bobot;
+    List<String> donatur = [];
+
+    bool updateRuning = false;
+
+    void create() {
+      sNamaHewan();
+      sJumlah();
+      sBobot();
+      sDonatur();
+    }
+
+    void updateMenu(){
+      print('');
+      print('Menu');
+      print('1. ubah nama');
+      print('2. ubah jumlah');
+      print('3. ubah bobot');
+      print('4. ubah donatur');
+      print('Pilih menu : \n');
+      var menu = stdin.readLineSync()!;
+      update(int.parse(menu));
+    }
+
+    void runUpdate(){
+      updateRuning = true;
+      while(updateRuning){
+        updateMenu();
+      }
+    }
+
+    void update(int menu) {
+      switch(menu){
+        case 1:
+          sNamaHewan();
+          updateMenu();
+        break;
+        case 2:
+          sJumlah();
+          updateMenu();
+        break;
+        case 3:
+          sBobot();
+          updateMenu();
+        break;
+        case 4:
+          sDonatur();
+          updateMenu();
+        break;
+        default:
+          updateRuning = false;
+        break;
+      }
+    }
+
+    void sNamaHewan() {
+      stdout.write('Masukkan nama hewan : ');
+      var _namaHewan = stdin.readLineSync()!;
+      if (jenisHewanQurban.contains(_namaHewan.toLowerCase())){
+        namaHewan = _namaHewan;
+      }else{
+        print('Hewan yang anda masukkan, tidak termasuk hewan qurban');
+      }
+    }
+
+    void sJumlah() {
+      stdout.write('Masukkan jumlah hewan : ');
+      jumlah = int.tryParse(stdin.readLineSync()!);
+    }
+
+    void sBobot() {
+      stdout.write('Masukkan bobot hewan : ');
+      bobot = int.tryParse(stdin.readLineSync()!);
+    }
+
+    void sDonatur() {
+      stdout.write('Masukkan jumlah donatur : ');
+      var _jumlahDonatiur = int.tryParse(stdin.readLineSync()??'')??0;
+      for (int i = 0 ; i < _jumlahDonatiur; i++) {
+        stdout.write('Masukkan nama donatur ke-${i+1} : ');
+        donatur.add(stdin.readLineSync()??'');
+      }
+    }
+
+    String cetak() {
+        return '$namaHewan, $jumlah, $bobot, ${donatur.toString().replaceAll('[', '').replaceAll(']', '')}';
+    }
+  }
+
+  // void main(){
+  //   var _listHewanQurban = <HewanQurban>[];
+
+  //   stdout.write('Berapa kelompok ? ');
+  //   var jpk = int.parse(stdin.readLineSync()!);
+  //   for (var i = 0; i < jpk; i++) {
+  //     stdout.write('\nDaftarkan hewan qurban anda \n');
+  //     var _hewanQurban = HewanQurban()
+  //     ..sNamaHewan()
+  //     ..sJumlah()
+  //     ..sBobot()
+  //     ..sDonatur();
+  //     _listHewanQurban.add(_hewanQurban);
+  //   }
+  //   _listHewanQurban.forEach((_hewan) {
+  //     _hewan.cetak();
+  //   });
+  // }
